7bf6c9fcc84ef9b131d6a18f404315fd988c7958
Paulo <32718867+paulocezargit@users.noreply.github.com>
2024-10-08 09:32:40 -0300

    Adding Functions
    This time, we've implemented the core functions that will allow us to manipulate the animation and images in our project.

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+let frame_current = 0;
+let frame_total_count = 0; 
+let frame_list = [];
+
+/**
+ * Adds images to the animation panel.
+ * 
+ * This function allows users to select and add multiple images 
+ * to the animation panel for further manipulation and playback.
+ * 
+ * @param {Array} images - An array of image URLs or image objects to be added.
+ * @returns {void}
+ */
+function AddImages(images) {
+
+}
+
+/**
+ * Updates the list of images in the correct order for display.
+ * 
+ * This function rearranges the current images based on the specified 
+ * criteria (e.g., user-defined order, timestamp).
+ * 
+ * @returns {void}
+ */
+function UpdateImages() {
+
+}
+
+/**
+ * Starts or resumes the animation of images.
+ * 
+ * This function begins the playback of the images in the current list.
+ * If the animation is already running, it will resume from the paused state.
+ * 
+ * @returns {void}
+ */
+function Play() {
+
+}
+
+/**
+ * Pauses the current animation.
+ * 
+ * This function halts the playback of the images, allowing the user 
+ * to resume later without losing their current position.
+ * 
+ * @returns {void}
+ */
+function Pause() {
+
+}
+
+/**
+ * Restarts the animation, returning to the first frame.
+ * 
+ * This function stops the current animation and resets the frame counter 
+ * to the beginning, preparing to play from the start.
+ * 
+ * @returns {void}
+ */
+function Stop() {
+
+}
+
+/**
+ * Removes a frame from the animation based on the provided index.
+ * 
+ * This function deletes a specific frame identified by its index 
+ * from the list of frames.
+ * 
+ * @param {number} key_frame - The index of the frame to be removed.
+ * @returns {void}
+ */
+function RemoveFrame(key_frame) {
+
+}
+
+/**
+ * Adds a new frame to the animation using the provided image.
+ * 
+ * This function inserts a new frame into the animation sequence, 
+ * using an image provided by the user.
+ * 
+ * @param {string|object} image - The image URL or image object to be added as a frame.
+ * @returns {void}
+ */
+function AddFrame(image) {
+
+}
+
+/**
+ * Clears all frames from the animation.
+ * 
+ * This function removes all frames from the current animation sequence,
+ * effectively resetting the animation state.
+ * 
+ * @returns {void}
+ */
+function ClearFrames() {
+
+}
